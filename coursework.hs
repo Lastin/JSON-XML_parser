80d83c06e9dd7932e57d5380079e4e63b530f362
Lastin <maksymsmail@gmail.com>
2015-11-13 13:07:03 +0000

    added almost all comments to code

--- coursework.hs
+++ coursework.hs
@@ -20,6 +20,24 @@ data JValue = JString String
             | JArray [JValue]
             deriving (Eq, Ord, Show)
 
+{-
+It converts elements from JSON string into objects type of JValue
+first it drops space characters from the end of a string
+then it drops the space characters from the head
+Next it converts the string into appropriate JValue types
+" - start of a string
+0 to 9 is a number
+t or f is boolean
+[ is array
+and { is objects
+If neither of the guards matches, then JNull is returned
+
+JArray stores the result of mapping every element returned from splitList with fromJSON
+splitList splits the string at commas and returns it as array of strings
+fromJSON is then called recursively
+
+JObject 
+-}
 fromJSON :: String -> JValue
 fromJSON (x:xs) | not (null xs) && isSpace (last xs) = fromJSON $ init (x:xs)
                 | isSpace x                          = fromJSON xs
@@ -33,21 +51,31 @@ fromJSON (x:xs) | not (null xs) && isSpace (last xs) = fromJSON $ init (x:xs)
                 | otherwise                          = JNull
                 where isSpace = (`elem` " \t\n\r\f\v")
 
+{-
+Function posForChar returns the next position of a separator outside an enclosed string, array or an object
+q - quotes
+e - escapes
+l - literals {[ or ]} 
+-}
 posForChar :: Integral a => Char -> String -> a -> a -> a -> a
 posForChar _ [] _ _ _ = 0
 posForChar c ('\\':ss) q e l = 1 + (posForChar c ss q (e + 1) l)
-posForChar c (s:ss) q e l | even e && l == 0 && s == '"' = 1 + (posForChar c ss (q + 1) 0 l)
- | even q && l == 0 && s == c = 0
- | even q && s `elem` "{[" = 1 + (posForChar c ss q 0 (l + 1))
- | even q && s `elem` "]}" = 1 + (posForChar c ss q 0 (l - 1))
- | otherwise = 1 + (posForChar c ss q 0 l)
+posForChar c (s:ss) q e l
+	| even e && l == 0 && s == '"' = 1 + (posForChar c ss (q + 1) 0 l)
+	| even q && l == 0 && s == c = 0
+	| even q && s `elem` "{[" = 1 + (posForChar c ss q 0 (l + 1))
+	| even q && s `elem` "]}" = 1 + (posForChar c ss q 0 (l - 1))
+	| otherwise = 1 + (posForChar c ss q 0 l)
 
 splitList :: Char -> String -> [String]
 splitList c "" = []
 splitList c s  = (take pos s:splitList c (drop (pos + 1) s))
                where pos = posForChar c s 0 0 0
 
--- TODO
+{-
+escapeChars function returns the escapes for most common special characters in the XML
+If a character is not escaped, it is returned as a string
+-}
 escapeChars :: Char -> String
 escapeChars c 
 	| c == '\"' = "&quot"
@@ -57,12 +85,33 @@ escapeChars c
 	| c == '>' = "&gt"
 	| otherwise = [c]
 
+{-
+prStr is a helper function for printing strings. It iterates through every character
+in the string, and calls escapeChars to escape special characters.
+-}
 prStr :: String -> String
 prStr s = foldl (++) "" [ escapeChars char | char <- s]
 
+{-
+indent is a helper function which returns number of tabs
+indent' returns same string as indent, but with new line character appended on head
+-}
 indent l = replicate l '\t'
 indent' l = "\n" ++ replicate l '\t'
 
+{-
+toXML' prints JValue as a formated XML string
+JObject is formated as <key>value<key> | (key, value) <- JObject
+JArray is formated as 
+	<array>
+		<item>JValue</item>
+		<item>JValue</item>
+		...
+		<item>JValue</item>
+	</array>
+
+toXML returs XML declaration followed by the result of toXML'
+-}
 toXML' :: JValue -> Int -> String
 toXML' (JString s) l = prStr s
 toXML' (JNumber n) l = show n
@@ -75,9 +124,13 @@ toXML' (JArray a) l = indent' l ++ foldl (++) "<array>" [indent' (l+1) ++ "<item
 toXML :: JValue -> String
 toXML x = "<?xml version=\"1.0\" encoding=\"utf-8\">" ++ toXML' x 0
 
-
+{-
+Function translate takes a JSON string and returns XML string
+it passes the result of fromJSON to toXML
+-}
 translate :: String -> String
 translate j = toXML $ fromJSON j
+
 
 main = do
 	[f, o, x] <- getArgs
@@ -85,11 +138,4 @@ main = do
 	let objs = fromJSON contents
 	writeFile o $ show objs
 	writeFile x $ translate contents
-
-{-
-main = do
-	[f, g] <- getArgs
-	contents <- readFile f
-	writeFile g $ translate contents-}
-	
 
